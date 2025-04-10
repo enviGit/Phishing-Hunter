e0c7f6d418ac7107be2d8b9326145d65b97acacd
Paweł Trojański <80624809+enviGit@users.noreply.github.com>
2025-04-10 15:28:00 +0200

    Reloading questions and checking answers implemented

--- Assets/Scripts/Core/OS/QuizManager.cs
+++ Assets/Scripts/Core/OS/QuizManager.cs
@@ -1,9 +1,10 @@
+using DG.Tweening;
 using ph.Managers;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 namespace ph.Core.OS {
@@ -35,16 +36,29 @@ namespace ph.Core.OS {
     }
 
     public class QuizManager : MonoBehaviour {
-        public TextAsset jsonFile;
-        public Transform workSpace;
-        public GameObject questionPrefab;
-        public float verticalSpacing = 75f;
-        public int maxDisplayedQuestions = 7;
+        [SerializeField] private TextAsset jsonFile;
+        [SerializeField] private Transform workSpace;
+        [SerializeField] private GameObject questionPrefab;
+        [SerializeField] private float verticalSpacing = 75f;
+        [SerializeField] private int maxDisplayedQuestions = 7;
+        [SerializeField] private GameObject resultPanel;
+        private TextMeshProUGUI resultText;
+        [SerializeField] private Button newButton;
+        [SerializeField] private Button checkButton;
         private List<QuizQuestion> questionList;
+        private List<int> displayedQuestionIds = new List<int>();
+        private int correctAnswers = 0;
+        private int totalQuestions = 0;
+        private Dictionary<int, string> userAnswers = new Dictionary<int, string>();
+        private List<int> incorrectQuestions = new List<int>();
 
         private void Start() {
+            resultText = resultPanel.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
             LoadQuestions();
             DisplayQuestions();
+            resultPanel.GetComponent<CanvasGroup>().alpha = 0;
+            checkButton.interactable = true;
+            newButton.interactable = false;
         }
 
         private void LoadQuestions() {
@@ -70,11 +84,15 @@ namespace ph.Core.OS {
             }
         }
         private void DisplayQuestions() {
-            List<QuizQuestion> filteredQuestions = questionList.Take(maxDisplayedQuestions).ToList();
+            // Pytania, które jeszcze nie zostały wyświetlone oraz pytania, które zostały odpowiedziane błędnie.
+            List<QuizQuestion> questionsToDisplay = questionList
+        .Where(q => !displayedQuestionIds.Contains(q.id) || incorrectQuestions.Contains(q.id))
+        .Where(q => !userAnswers.ContainsKey(q.id) || userAnswers[q.id] != q.correctAns)
+        .ToList();
 
             float currentY = -5f;
 
-            foreach (var question in filteredQuestions) {
+            foreach (var question in questionsToDisplay) {
                 GameObject questionItem = Instantiate(questionPrefab, workSpace.GetChild(0));
                 RectTransform rectTransform = questionItem.GetComponent<RectTransform>();
                 rectTransform.localPosition = new Vector3(0, currentY, 0);
@@ -91,6 +109,119 @@ namespace ph.Core.OS {
                 ansBText.text = "B: " + question.ansB;
                 ansCText.text = "C: " + question.ansC;
                 ansDText.text = "D: " + question.ansD;
+
+                Toggle ansAToggle = questionItem.transform.GetChild(3).GetChild(0).GetComponent<Toggle>();
+                Toggle ansBToggle = questionItem.transform.GetChild(3).GetChild(1).GetComponent<Toggle>();
+                Toggle ansCToggle = questionItem.transform.GetChild(3).GetChild(2).GetComponent<Toggle>();
+                Toggle ansDToggle = questionItem.transform.GetChild(3).GetChild(3).GetComponent<Toggle>();
+
+                ansAToggle.onValueChanged.AddListener((isSelected) => SetAnswer(question.id, isSelected, "A"));
+                ansBToggle.onValueChanged.AddListener((isSelected) => SetAnswer(question.id, isSelected, "B"));
+                ansCToggle.onValueChanged.AddListener((isSelected) => SetAnswer(question.id, isSelected, "C"));
+                ansDToggle.onValueChanged.AddListener((isSelected) => SetAnswer(question.id, isSelected, "D"));
+
+                displayedQuestionIds.Add(question.id);
+                totalQuestions++;
+            }
+        }
+        public void CheckAnswers() {
+            checkButton.interactable = false;
+
+            foreach (var question in questionList.Where(q => displayedQuestionIds.Contains(q.id))) {
+                string selectedAnswer = GetSelectedAnswerForQuestion(question.id);
+                bool isCorrect = selectedAnswer == question.correctAns;
+
+                if (!isCorrect) {
+                    incorrectQuestions.Add(question.id);
+                }
+
+                UpdateToggleColor(selectedAnswer, isCorrect);
+
+                if (isCorrect) {
+                    correctAnswers++;
+                }
+            }
+
+            string result = $"{correctAnswers} / {totalQuestions} ({(correctAnswers / (float)totalQuestions * 100):0.0}%)";
+            resultText.text = result;
+
+            resultPanel.GetComponent<CanvasGroup>().DOFade(1, 0.5f);
+            newButton.interactable = true;
+        }
+        public void LoadNewQuestions() {
+            newButton.interactable = false;
+            checkButton.interactable = true;
+
+            foreach (Transform child in workSpace.GetChild(0)) {
+                Destroy(child.gameObject);
+            }
+
+            displayedQuestionIds.Clear();
+            correctAnswers = 0;
+            totalQuestions = 0;
+            incorrectQuestions.Clear();
+
+            DisplayQuestions();
+            resultPanel.GetComponent<CanvasGroup>().DOFade(0, 0.5f);
+        }
+        private void SetAnswer(int questionId, bool isSelected, string answer) {
+            if (isSelected) {
+                if (userAnswers.ContainsKey(questionId)) {
+                    userAnswers[questionId] = answer;
+                }
+                else {
+                    userAnswers.Add(questionId, answer);
+                }
+            }
+            else {
+                if (userAnswers.ContainsKey(questionId) && userAnswers[questionId] == answer) {
+                    userAnswers.Remove(questionId);
+                }
+            }
+        }
+        private string GetSelectedAnswerForQuestion(int questionId) {
+            if (userAnswers.ContainsKey(questionId)) {
+                return userAnswers[questionId];
+            }
+            else {
+                return "";
+            }
+        }
+        private void UpdateToggleColor(string selectedAnswer, bool isCorrect) {
+            foreach (Transform questionItem in workSpace.GetChild(0)) {
+                Toggle ansAToggle = questionItem.GetChild(3).GetChild(0).GetComponent<Toggle>();
+                Toggle ansBToggle = questionItem.GetChild(3).GetChild(1).GetComponent<Toggle>();
+                Toggle ansCToggle = questionItem.GetChild(3).GetChild(2).GetComponent<Toggle>();
+                Toggle ansDToggle = questionItem.GetChild(3).GetChild(3).GetComponent<Toggle>();
+
+                ColorBlock colorBlock = new ColorBlock {
+                    normalColor = isCorrect ? Color.green : Color.red,
+                    highlightedColor = isCorrect ? Color.green : Color.red,
+                    pressedColor = isCorrect ? Color.green : Color.red,
+                    selectedColor = isCorrect ? Color.green : Color.red,
+                    disabledColor = isCorrect ? Color.green : Color.red,
+                    colorMultiplier = 1f
+                };
+
+                switch (selectedAnswer) {
+                    case "A":
+                        ansAToggle.colors = colorBlock;
+                        break;
+                    case "B":
+                        ansBToggle.colors = colorBlock;
+                        break;
+                    case "C":
+                        ansCToggle.colors = colorBlock;
+                        break;
+                    case "D":
+                        ansDToggle.colors = colorBlock;
+                        break;
+                }
+
+                ansAToggle.interactable = false;
+                ansBToggle.interactable = false;
+                ansCToggle.interactable = false;
+                ansDToggle.interactable = false;
             }
         }
     }
